53cbd18e71509a7fe2c12f588cd05c7d35f9f7e1
T3r1jj <t3r1jj@gmail.com>
2015-11-16 23:54:02 +0100

    First commit, ideas

---
+++ src/ztppro/ZtpPro.java
@@ -0,0 +1,16 @@
+package ztppro;
+
+/**
+ *
+ * @author Damian Terlecki
+ */
+public class ZtpPro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
