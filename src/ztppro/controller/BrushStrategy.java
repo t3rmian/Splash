49750294f9645f7c7b07f0111c9b751115a51971
T3r1jj <t3r1jj@gmail.com>
2015-11-25 01:29:30 +0100

    Paintbrush fix

--- src/ztppro/controller/BrushStrategy.java
+++ src/ztppro/controller/BrushStrategy.java
@@ -27,9 +27,9 @@ class BrushStrategy extends PencilStrategy {
             Graphics2D g2d = (Graphics2D) controller.getModel().getImage().getGraphics();
             g2d.setColor(firstColor);
             for (Point2D point : new Line2DAdapter(lastEvent.getX(), lastEvent.getY(), currentEvent.getX(), currentEvent.getY())) {
-                g2d.fillOval((int) point.getX() - halfRadius, (int) point.getY() - halfRadius, radius, radius);
+                g2d.fillOval((int) point.getX() - radius, (int) point.getY() - radius, 2 * radius, 2 * radius);
             }
-            controller.getView().repaint(Math.min(lastEvent.getX(), currentEvent.getX()) - halfRadius, Math.min(lastEvent.getY(), currentEvent.getY()) - halfRadius, Math.abs(currentEvent.getX() - lastEvent.getX()) + 1 + radius, Math.abs(currentEvent.getY() - lastEvent.getY()) + 1 + radius);
+            controller.getView().repaint(Math.min(lastEvent.getX(), currentEvent.getX()) - radius, Math.min(lastEvent.getY(), currentEvent.getY()) - radius, Math.abs(currentEvent.getX() - lastEvent.getX()) + 1 + 2 * radius, Math.abs(currentEvent.getY() - lastEvent.getY()) + 1 + 2 * radius);
         }
     }
 
@@ -39,7 +39,7 @@ class BrushStrategy extends PencilStrategy {
         Graphics2D g2d = (Graphics2D) controller.getModel().getImage().getGraphics();
         g2d.setColor(firstColor);
 
-        g2d.fillOval(e.getX() - halfRadius, e.getY() - halfRadius, radius, radius);
-        controller.getView().repaint(e.getX() - halfRadius, e.getY() - halfRadius, radius, radius);
+        g2d.fillOval(e.getX() - radius, e.getY() - radius, 2 * radius, 2 * radius);
+        controller.getView().repaint(e.getX() - radius, e.getY() - radius, 2 * radius, 2 * radius);
     }
 }
